93bb77f67cb78f8f998b8ffa9771e919427b331c
Kyle Cesmat <kyle.cesmat@formidable.com>
2018-04-29 22:47:42 -0700

    generate docs

---
+++ webpack.demo.build.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+
+  entry: ['./demo/app.js'],
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+    modules: [path.join(__dirname, 'node_modules')]
+  },
+
+  output: {
+    path: path.join(__dirname, 'docs')
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'React Cheers - A lightweight toast manager'
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      inline: ['demo']
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: 'url-loader?limit=8192'
+      }
+    ]
+  }
+};
